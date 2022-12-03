ae0f38ac95806b052c12dddaeaabf49acf761f17
TopchikTyomchik <101976747+TopchikTyomchik@users.noreply.github.com>
2022-12-03 13:19:23 +0300

    timeout

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
 let items = document.querySelectorAll("h3");   
 let countdownElement = document.querySelector(".countdown");   
-const countdownTime = new Date(2035, 11, 2, 23, 50, 2, 0).getTime();
+const countdownTime = new Date(2022, 11, 3, 13, 18, 59, 0).getTime();
 
 function getCountDown() {
    const presentTime = new Date().getTime();
    const leftTime = countdownTime - presentTime;
+
+   if (leftTime < 0) {
+      clearInterval(interval);
+      countdownElement.innerHTML = `<h3 class=".timeout">Время вышло</h3>`
+   };
    
 /* 1s = 1000ms
    1m =  60s
@@ -24,10 +29,7 @@ function getCountDown() {
 
    items.forEach((item, index) => item.innerHTML = values[index]);
 
-   if (leftTime < 0) {
-      clearInterval(interval);
-      countdownElement.innerHTML = `<h3 class=".timeout">Время вышло</h3>`
-   };
+   
 }
 
 getCountDown();
